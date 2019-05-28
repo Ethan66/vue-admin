8f19019731173da43ac8fe69ed16146c2f98ee24
zhounie <iszhounie@aliyun.com>
2019-05-28 14:41:48 +0800

    删除多余文件

--- src/pages/auth-config/funclimit/roleLimit.vue
+++ src/pages/auth-config/funclimit/roleLimit.vue
@@ -86,8 +86,7 @@
 <script>
 import classify from '@/pages/auth-config/funclimit/components/classify'
 import methods from './methods'
-import dialogConfig from './dialogConfig'
-import typeDialog from './typeDialog'
+import typeDialog from './components/typeDialog'
 import { apiGetAllPermissionResource, apiSetRolePermissionResource, apiGetRolePermissionFields, apiSetRolePermissionFields } from '@/api/role'
 
 const initFormItem = [
@@ -106,9 +105,42 @@ const initRoleFormItem = [
 export default {
   name: 'role-limit',
   components: { classify, typeDialog },
-  mixins: [methods, dialogConfig],
+  mixins: [methods],
   data () {
+    let checkSortNo = (rule, value, callback) => {
+      let reg = /^[0-9]+$/
+      if (!reg.test(value)) {
+        callback(new Error('请输入正确的数值'))
+      } else {
+        callback()
+      }
+    }
     return {
+      typeDialogTitle: '编辑类型',
+      formData: {
+        cloneRoleIds: []
+      },
+      typeDialogRules: {
+        resourceParentId: [
+          { required: true, message: '请选择所属分类', trigger: 'blur' }
+        ],
+        roleName: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { max: 20, message: '最多输入20个字符', trigger: 'blur' }
+        ],
+        sortNo: [
+          { required: true, message: '请输入显示排序', trigger: 'blur' },
+          { validator: checkSortNo, message: '请输入正确的数值', trigger: ['blur', 'change'] }
+        ]
+      },
+      typeDialogBtn: [
+        { label: '取 消', type: 'delete', clickfn: 'handleTypeDialogRefuse' },
+        { label: '确 认', type: 'edit', color: 'primary', clickfn: 'handleTypeDialogSubmit' }
+      ],
+      formItem: [],
+      typeDialogVisible: false,
+      isEdit: false,
+      isClassify: 0, // 0：角色，1：角色分类
       classifyList: [], // 角色数据
       menuList: [], // 菜单列表
       resultChecked: {}, // 传给后端的结果
--- src/pages/auth-config/funclimit/staffRole.vue
+++ src/pages/auth-config/funclimit/staffRole.vue
@@ -63,16 +63,15 @@
 <script>
 import { staffRole } from './mixins'
 import methods from './methods'
-import dialogConfig from './dialogConfig.js'
 import basicMethod from '@/config/mixins'
-import typeDialog from './typeDialog'
+import typeDialog from './components/typeDialog'
 import classify from './components/classify'
 import staffDialog from './components/staffDialog'
 import { apiPageQueryUserRole } from '@/api/role'
 
 export default {
   name: 'staff-role',
-  mixins: [methods, basicMethod, staffRole, dialogConfig],
+  mixins: [methods, basicMethod, staffRole],
   created () {
     this.handleGetTableData(apiPageQueryUserRole)
     // 获取角色分类树
@@ -83,7 +82,40 @@ export default {
     this.handleApiListConsoleUser()
   },
   data () {
+    let checkSortNo = (rule, value, callback) => {
+      let reg = /^[0-9]+$/
+      if (!reg.test(value)) {
+        callback(new Error('请输入正确的数值'))
+      } else {
+        callback()
+      }
+    }
     return {
+      typeDialogTitle: '编辑类型',
+      formData: {
+        cloneRoleIds: []
+      },
+      typeDialogRules: {
+        resourceParentId: [
+          { required: true, message: '请选择所属分类', trigger: 'blur' }
+        ],
+        roleName: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { max: 20, message: '最多输入20个字符', trigger: 'blur' }
+        ],
+        sortNo: [
+          { required: true, message: '请输入显示排序', trigger: 'blur' },
+          { validator: checkSortNo, message: '请输入正确的数值', trigger: ['blur', 'change'] }
+        ]
+      },
+      typeDialogBtn: [
+        { label: '取 消', type: 'delete', clickfn: 'handleTypeDialogRefuse' },
+        { label: '确 认', type: 'edit', color: 'primary', clickfn: 'handleTypeDialogSubmit' }
+      ],
+      formItem: [],
+      typeDialogVisible: false,
+      isEdit: false,
+      isClassify: 0, // 0：角色，1：角色分类
       defaultSearchObj: { a: 1 },
       selectStaff: [],
       classifyList: [],
