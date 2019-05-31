02155889773113492b9a233d854c1f8da8f60b03
zc <zhuangcheng@ldxinyong.com>
2019-05-31 18:06:56 +0800

    删除test字段

--- src/configureData/menu.js
+++ src/configureData/menu.js
@@ -2,94 +2,49 @@ const config = {
   'version_3.0.0': {
     catalogue: [
       // { menuName: '权限管理', code: 'auth-config', menuUrl: '/auth-config', sortNo: 1 },
-      // { menuName: '开发中心', code: 'develop-center', menuUrl: '/develop-center', sortNo: 2 },
-      // { menuName: '日志管理', code: 'log-manage', menuUrl: '/log', sortNo: 3 }
+      ['测试目录1', 'test-menu1', '/test-catalogue1'],
+      ['测试目录2', 'test-menu2', '/test-catalogue2']
     ],
     menu: [
       // // 权限管理
-      // { menuName: '组织架构', menuParentName: '权限管理', code: 'organization', menuUrl: '/auth-config/organization/index', sortNo: 1 },
-      // { menuName: '员工管理', menuParentName: '权限管理', code: 'staff-manage', menuUrl: '/auth-config/staff/index', sortNo: 2 },
-      // { menuName: '功能权限管理', menuParentName: '权限管理', code: 'config-manage', menuUrl: '/auth-config/funclimit/index', sortNo: 3 },
-      // { menuName: '访问控制', menuParentName: '权限管理', code: 'visit-config', menuUrl: '/auth-config/visit-control/ip-control/index', sortNo: 4 },
-      // // 开发中心
-      // { menuName: '菜单管理', menuParentName: '开发中心', code: 'menu-manage', menuUrl: '/develop-center/menu-manage/index', sortNo: 1 },
-      // { menuName: '页面管理', menuParentName: '开发中心', code: 'page-manage', menuUrl: '/develop-center/page-manage/index', sortNo: 2 },
-      // // 日志管理
-      // { menuName: '登录日志', menuParentName: '日志管理', code: 'login-log', menuUrl: '/log/login-log/index', sortNo: 1 },
-      // { menuName: '异常日志', menuParentName: '日志管理', code: 'error-log', menuUrl: '/log/error-log/index', sortNo: 2 }
+      // { menuParentName: '权限管理', menuName: '组织架构', code: 'organization', menuUrl: '/auth-config/organization/index', sortNo: 1 },
+      ['测试目录1', '测试菜单1-1', 'test-menu1-1', '/test-catalogue1/1'],
+      ['测试目录1', '测试菜单1-2', 'test-menu1-2', '/test-catalogue1/2'],
+      ['测试目录2', '测试菜单2-1', 'test-menu2-1', '/test-catalogue2/1'],
+      ['测试目录2', '测试菜单2-2', 'test-menu2-2', '/test-catalogue2/2']
     ],
     btn: [
       // // 功能权限管理
-      // { menuName: '编 辑', menuParentName: '权限管理', code: 'config-manage-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '删 除', menuParentName: '权限管理', code: 'config-manage-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '添加员工', menuParentName: '权限管理', code: 'config-manage-add', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // // 员工管理
-      // { menuName: '新建员工', menuParentName: '员工管理', code: 'staff-manage-add', menuUrl: '/pilianganniu1-1', sortNo: 1 }
-      { menuName: '停用', menuParentName: '员工管理', code: 'staff-batch-stop', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      { menuName: '启用', menuParentName: '员工管理', code: 'staff-batch-start', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      { menuName: '禁止登录', menuParentName: '员工管理', code: 'staff-batch-stop-log', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      { menuName: '允许登录', menuParentName: '员工管理', code: 'staff-batch-ok-log', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // 功能权限管理
-      { menuName: '删除', menuParentName: '功能权限管理', code: 'config-batch-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 }
-      // // 菜单管理
-      // { menuName: '新增菜单', menuParentName: '菜单管理', code: 'menu-add-menu', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '批量新建目录', menuParentName: '菜单管理', code: 'menu-add-catogue-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '批量新建菜单', menuParentName: '菜单管理', code: 'menu-add-menu-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '批量新建按钮', menuParentName: '菜单管理', code: 'menu-add-btn-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '菜单管理', code: 'menu-edit-menu', menuUrl: '/pilianganniu1-2', sortNo: 2 },
-      // { menuName: '更多', menuParentName: '菜单管理', code: 'menu-more', menuUrl: '/pilianganniu1-2', sortNo: 2 },
-      // { menuName: '新建平级菜单', menuParentName: '菜单管理', code: 'menu-add-same-level', menuUrl: '/pilianganniu2-1', sortNo: 1 },
-      // { menuName: '新建下级菜单', menuParentName: '菜单管理', code: 'menu-add-next-level', menuUrl: '/pilianganniu2-1', sortNo: 1 },
-      // { menuName: '删除', menuParentName: '菜单管理', code: 'menu-delete', menuUrl: '/pilianganniu2-1', sortNo: 1 },
-      // { menuName: '详情', menuParentName: '菜单管理', code: 'menu-detail', menuUrl: '/pilianganniu2-1', sortNo: 1 },
-      // // 页面管理
-      // { menuName: '新建页面', menuParentName: '页面管理', code: 'page-add-page', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '获取全部menuList', menuParentName: '页面管理', code: 'page-get-menu', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '批量新建页面', menuParentName: '页面管理', code: 'page-add-page-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '获取所有页面', menuParentName: '页面管理', code: 'page-get-page-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '批量新建字段', menuParentName: '页面管理', code: 'page-add-tybe-all', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '页面管理', code: 'page-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // // 字段管理
-      // { menuName: '快速生成', menuParentName: '页面管理', code: 'tybe-fast-create', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '手动添加', menuParentName: '页面管理', code: 'tybe-hand-create', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '删除', menuParentName: '页面管理', code: 'tybe-hand-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '返回列表', menuParentName: '页面管理', code: 'tybe-go-back', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '页面管理', code: 'tybe-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '取消', menuParentName: '页面管理', code: 'tybe-cancel', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '删除', menuParentName: '页面管理', code: 'tybe-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-
-      // // 组织架构
-      // { menuName: '新增部门', menuParentName: '组织架构', code: 'organization-add', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '组织架构', code: 'organization-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '更多', menuParentName: '组织架构', code: 'organization-more', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '新建平级部门', menuParentName: '组织架构', code: 'organization-add-same-level', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '新建下级部门', menuParentName: '组织架构', code: 'organization-add-next-level', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '停用', menuParentName: '组织架构', code: 'organization-stop', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '删除', menuParentName: '组织架构', code: 'organization-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 },    
-      // // ip授权
-      // { menuName: '启 用', menuParentName: '访问控制', code: 'visit-ip-open', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '启 用', menuParentName: '访问控制', code: 'visit-ip-open-batch', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '停 用', menuParentName: '访问控制', code: 'visit-ip-stop', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '停 用', menuParentName: '访问控制', code: 'visit-ip-stop-batch', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '添加IP', menuParentName: '访问控制', code: 'visit-ip-add-ip', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编 辑', menuParentName: '访问控制', code: 'visit-ip-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // // 账号授权
-      // { menuName: '失 效', menuParentName: '访问控制', code: 'visit-c-stop', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '失 效', menuParentName: '访问控制', code: 'visit-c-stop-batch', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '添加授权', menuParentName: '访问控制', code: 'visit-c-add', menuUrl: '/pilianganniu1-1', sortNo: 1 }
-      // { menuName: '新建员工', menuParentName: '员工管理', code: 'staff-manage', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '员工管理', code: 'staff-admin-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '停用', menuParentName: '员工管理', code: 'staff-admin-stop', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '启用', menuParentName: '员工管理', code: 'staff-admin-open', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '禁止登陆', menuParentName: '员工管理', code: 'staff-admin-nologin', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '允许登录', menuParentName: '员工管理', code: 'staff-admin-login', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '重置密码', menuParentName: '员工管理', code: 'staff-admin-reset', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '编辑', menuParentName: '功能权限管理', code: 'config-manage-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
-      // { menuName: '删除', menuParentName: '功能权限管理', code: 'config-manage-delete', menuUrl: '/pilianganniu1-1', sortNo: 1 },
+      // { menuParentName: '权限管理', menuName: '编 辑', code: 'config-manage-edit', menuUrl: '/pilianganniu1-1', sortNo: 1 },
+      ['测试菜单1-1', '按钮1-1-1', 'btn1-1-1'],
+      ['测试菜单1-1', '按钮1-1-2', 'btn1-1-2'],
+      ['测试菜单1-1', '按钮1-1-3', 'btn1-1-3'],
+      ['测试菜单1-2', '按钮1-2-1', 'btn1-2-1'],
+      ['测试菜单1-2', '按钮1-2-2', 'btn1-2-2'],
+      ['测试菜单1-2', '按钮1-2-3', 'btn1-2-3'],
+      ['测试菜单2-1', '按钮2-1-1', 'btn2-1-1'],
+      ['测试菜单2-1', '按钮2-1-2', 'btn2-1-2'],
+      ['测试菜单2-1', '按钮2-1-3', 'btn2-1-3'],
+      ['测试菜单2-2', '按钮2-2-1', 'btn2-2-1'],
+      ['测试菜单2-2', '按钮2-2-2', 'btn2-2-2'],
+      ['测试菜单2-2', '按钮2-2-3', 'btn2-2-3']
     ]
   }
 }
 
 const version = '3.0.0'
 
-export default config[`version_${version}`]
+function transformData (data) {
+  data.catalogue = data.catalogue.map((item, i) => {
+    return { menuName: item[0], code: item[1], menuUrl: item[2], sortNo: i + 1 }
+  })
+  data.menu = data.menu.map((item, i) => {
+    return { menuParentName: item[0], menuName: item[1], code: item[2], menuUrl: item[3], sortNo: i + 1 }
+  })
+  data.btn = data.btn.map((item, i) => {
+    return { menuParentName: item[0], menuName: item[1], code: item[2], menuUrl: '/btn', sortNo: i + 1 }
+  })
+  return data
+}
+
+export default transformData(config[`version_${version}`])
--- src/configureData/pageKey.js
+++ src/configureData/pageKey.js
@@ -1,116 +1,11 @@
 const config = {
   'version_3.0.0': {
     page: [
-      { menuName: '菜单管理', pageCode: 'menu-manage1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '页面管理', pageCode: 'page-manage1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '页面管理', pageCode: 'tybe-manage1', pageName: '字段管理', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '页面管理', pageCode: 'tybe-manage2', pageName: '快速生成字段', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '功能权限管理', pageCode: 'function-limit1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '访问控制', pageCode: 'visit-manage2', pageName: 'IP控制', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '访问控制', pageCode: 'visit-manage1', pageName: '账户控制', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '员工管理', pageCode: 'staff-manage1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '组织架构', pageCode: 'organization-manage1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '登录日志', pageCode: 'log-login1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
-      { menuName: '异常日志', pageCode: 'error-log1', pageName: '', pageUrl: '/default-url', pageStatus: 0 }
+      // { menuName: '菜单管理', pageCode: 'menu-manage1', pageName: '', pageUrl: '/default-url', pageStatus: 0 },
     ],
     'tybe': [
       // 菜单管理
       // { pageCode: 'menu-manage1', fieldName: '菜单名称', fieldValue: 'menuName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '类型', fieldValue: 'menuType', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '层级', fieldValue: 'menuLevel', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '编码', fieldValue: 'code', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '显示排序', fieldValue: 'sortNo', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '状态', fieldValue: 'status', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'menu-manage1', fieldName: '请求地址', fieldValue: 'menuUrl', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // // 页面管理
-      // { pageCode: 'page-manage1', fieldName: '页面名称', fieldValue: 'pageName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '页面编码', fieldValue: 'pageCode', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '地址', fieldValue: 'pageUrl', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '关联菜单编码', fieldValue: 'menuCode', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '创建人', fieldValue: 'userName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '描述', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'page-manage1', fieldName: '状态', fieldValue: 'pageStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // // 字段管理
-      // { pageCode: 'tybe-manage1', fieldName: '字段名称', fieldValue: 'fieldName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '字段', fieldValue: 'fieldValue', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '在此之前固定列', fieldValue: 'fixedStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '是否允许用户设置', fieldValue: 'setStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '是否隐藏', fieldValue: 'displayStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '是否必填', fieldValue: 'fieldRequired', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage1', fieldName: '显示排序', fieldValue: 'fieldSort', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // // 快速生成字段
-      // { pageCode: 'tybe-manage2', fieldName: '字段名称', fieldValue: 'fieldName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage2', fieldName: '字段', fieldValue: 'fieldValue', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage2', fieldName: '是否必填', fieldValue: 'fieldRequired', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage2', fieldName: '是否允许用户设置', fieldValue: 'setStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'tybe-manage2', fieldName: '是否隐藏', fieldValue: 'displayStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // 组织架构
-      // { pageCode: 'organization-manage1', fieldName: '部门名称', fieldValue: 'departmentName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'organization-manage1', fieldName: '人数', fieldValue: 'departmentCount', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'organization-manage1', fieldName: '类型', fieldValue: 'departmentType', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'organization-manage1', fieldName: '显示排序', fieldValue: 'sortNo', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'organization-manage1', fieldName: '负责人', fieldValue: 'directorName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'organization-manage1', fieldName: '状态', fieldValue: 'departmentStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // 登陆日志
-      // { pageCode: 'log-login1', fieldName: '姓名', fieldValue: 'userName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '部门', fieldValue: 'departmentName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '终端', fieldValue: 'terminalType', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '操作系统', fieldValue: 'operateSystem', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: 'IP', fieldValue: 'loginIp', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '状态', fieldValue: 'loginStatus', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '操作信息', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'log-login1', fieldName: '登录时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // // 异常日志
-      // { pageCode: 'error-log1', fieldName: '姓名', fieldValue: 'realName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'error-log1', fieldName: '操作类型', fieldValue: 'opTypeDesc', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'error-log1', fieldName: 'IP', fieldValue: 'id', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'error-log1', fieldName: '状态', fieldValue: 'opStatusDesc', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'error-log1', fieldName: '操作信息', fieldValue: 'opResult', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'error-log1', fieldName: '创建时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // 账户控制
-      { pageCode: 'visit-manage1', fieldName: '姓名', fieldValue: 'userRealName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '部门', fieldValue: 'departmentName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '创建人', fieldValue: 'createrName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '授权时间', fieldValue: 'gmtModified', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '描述', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '状态', fieldValue: 'isDeleteMsg', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage1', fieldName: '创建时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // IP控制
-      { pageCode: 'visit-manage2', fieldName: 'IP地址', fieldValue: 'addressIp', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage2', fieldName: '创建人', fieldValue: 'creater', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage2', fieldName: '状态', fieldValue: 'isDeleteName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage2', fieldName: '描述', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'visit-manage2', fieldName: '创建时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 }
-    ]
-  },
-  'version_2.0.0': {
-    page: [
-      { menuName: '批量菜单10-1', pageCode: 'page-test10', pageName: '', pageUrl: '/default-url', pageStatus: 1 },
-      { menuName: '批量菜单11-1', pageCode: 'page-test11', pageName: '', pageUrl: '/default-url', pageStatus: 1 },
-      { menuName: '批量菜单11-1', pageCode: 'page-test12', pageName: '批量菜单11-1-1', pageUrl: '/default-url', pageStatus: 1 },
-    ],
-    tybe: [
-      { pageCode: 'page-test10', fieldName: '名字1', fieldValue: 'name1', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'page-test10', fieldName: '名字2', fieldValue: 'name2', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'page-test11', fieldName: '名字3', fieldValue: 'name4', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'page-test11', fieldName: '名字4', fieldValue: 'name5', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'page-test12', fieldName: '名字5', fieldValue: 'name5', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      { pageCode: 'page-test12', fieldName: '名字6', fieldValue: 'name6', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '姓名', fieldValue: 'userRealName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '部门', fieldValue: 'departmentName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '创建人', fieldValue: 'createrName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '授权时间', fieldValue: 'gmtModified', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '描述', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '状态', fieldValue: 'isDeleteMsg', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '创建时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // // IP控制
-      // { pageCode: 'visit-manage1', fieldName: 'IP地址', fieldValue: 'addressIp', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '创建人', fieldValue: 'creater', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '状态', fieldValue: 'isDeleteName', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '描述', fieldValue: 'remark', fieldRequired: 1, displayStatus: 0, setStatus: 0 },
-      // { pageCode: 'visit-manage1', fieldName: '创建时间', fieldValue: 'gmtCreate', fieldRequired: 1, displayStatus: 0, setStatus: 0 }
-      // { "pageCode": "staff-manage1", "fieldName": "账户", "fieldValue": "userName", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "姓名", "fieldValue": "realName", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "手机号", "fieldValue": "telephone", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "部门名称", "fieldValue": "departmentName", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "职位", "fieldValue": "position", "fieldRequired": 1, "displayStatus": 0, "setStatus": 4 }, { "pageCode": "staff-manage1", "fieldName": "汇报对象", "fieldValue": "reportToName", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "状态", "fieldValue": "statusMsg", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "状态", "fieldValue": "status", "fieldRequired": 1, "displayStatus": 1, "setStatus": 1 }, { "pageCode": "staff-manage1", "fieldName": "角色", "fieldValue": "roleName", "fieldRequired": 1, "displayStatus": 0, "setStatus": 1 },
-      // {"pageCode":"function-limit1","fieldName":"姓名","fieldValue":"realName","fieldRequired":1,"displayStatus":0,"setStatus":1},{"pageCode":"function-limit1","fieldName":"手机号","fieldValue":"telephone","fieldRequired":1,"displayStatus":0,"setStatus":1},{"pageCode":"function-limit1","fieldName":"部门","fieldValue":"departmentName","fieldRequired":1,"displayStatus":0,"setStatus":1},{"pageCode":"function-limit1","fieldName":"职位","fieldValue":"position","fieldRequired":1,"displayStatus":0,"setStatus":4},{"pageCode":"function-limit1","fieldName":"角色","fieldValue":"roleName","fieldRequired":1,"displayStatus":0,"setStatus":1}
     ]
   }
 }
