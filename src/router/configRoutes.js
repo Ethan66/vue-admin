b3950cd6f7d03e50522d302b066c8801dbed6fd7
zc <zhuangcheng@ldxinyong.com>
2019-06-06 16:37:46 +0800

    update

--- src/router/configRoutes.js
+++ src/router/configRoutes.js
@@ -7,26 +7,18 @@ export default [
       { path: 'index', component: () => import('@/common/home'), meta: { title: '首页' } }
     ]
   },
-  {
-    path: '/main/develop-center/menu-manage',
-    component: Layout,
-    children: [
-      { path: 'newpage', component: () => import('@/pages/develop-center/menu-manage/newpage'), meta: { title: '新页面', level: 3 } },
-      { path: 'fourpage', component: () => import('@/pages/develop-center/menu-manage/fourpage'), meta: { title: '四级页面', level: 4 } }
-    ]
-  },
-  {
-    path: '/main/develop-center/page-manage',
-    component: Layout,
-    children: [
-      { path: 'tybemanage', component: () => import('@/pages/develop-center/page-manage/tybemanage'), meta: { title: '字段页面', level: 3 } }
-    ]
-  },
-  {
+  /* {
     path: '/develop-center',
     component: Layout,
     children: [
-      { path: '/develop-center/page-manage/tybemanage', component: () => import('@/pages/develop-center/page-manage/tybemanage'), name: 'tybe-manage', meta: { title: '字段管理' } }
+      { path: '/develop-center/menu-manage/newpage', component: () => import('@/pages/develop-center/menu-manage/newpage'), meta: { title: '动态展示副标签' } }
+    ]
+  }, */
+  {
+    path: '/develop-center/page-manage',
+    component: Layout,
+    children: [
+      { path: 'tybemanage', component: () => import('@/pages/develop-center/page-manage/tybemanage'), meta: { title: '字段页面' } }
     ]
   }
 ]
