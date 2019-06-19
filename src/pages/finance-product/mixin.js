75992dfa9161a34e6e4ea5cbf583d5e475dbc1ce
zc <zhuangcheng@ldxinyong.com>
2019-06-19 10:09:33 +0800

    删除金融产品多余配置

--- src/pages/finance-product/mixin.js
+++ src/pages/finance-product/mixin.js
@@ -1,4 +1,4 @@
-import { basicInitObj, searchTableInitObj, tableInitObj, dialogInitObj } from '@/components/basicObj'
+import { basicInitObj, tableInitObj, dialogInitObj } from '@/components/basicObj'
 import { authMoreBtn } from '@/components/methods'
 
 const tybeObj = JSON.parse(sessionStorage.getItem('tybeObj') || '{}')
@@ -69,64 +69,5 @@ export const shapeHistory = {
       { label: '创建时间', prop: 'gmtCreate', type: 'cell', width: 80 },
       { label: '操作', prop: 'btn', type: 'btn', width: 80 }
     ]
-  }
-}
-
-// 字段管理
-export const tybeManage = {
-  data () {
-    return this.$setBtnConfig(JSON.parse(JSON.stringify(searchTableInitObj)), [{ edit: { code: 'tybe-edit' } }, { cancel: { show: false } }, { delete: { code: 'tybe-delete', show: false } }])
-  },
-  created () {
-    let configSearchItem = ['fieldName', 'fieldValue']
-    let configTableItem = {
-      selection: 50,
-      fieldValue: { clsName: 'cm-btn-color', width: 80 },
-      fieldName: { width: 90, type: 'input', canEdit: 1 },
-      displayStatus: { width: 90, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      fieldRequired: { width: 100, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      setStatus: { width: 140, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      fieldSort: { width: 90, type: 'input', canEdit: 1 },
-      fixedStatus: { width: 140, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      btn: 116
-    }
-    this.searchItem = this.$setItem(tybeObj['tybe-manage1'], configSearchItem, 'search')
-    this.tableItem = this.$setItem(tybeObj['tybe-manage1'], configTableItem, 'table')
-    if (this.tableBtn.filter(item => item.show).length === 0) {
-      this.tableItem.splice(this.tableItem.length - 1, 1)
-    }
-  }
-}
-
-// 快速创建字段
-export const fastCreateType = {
-  data () {
-    return this.$setBtnConfig(JSON.parse(JSON.stringify(tableInitObj)), [{ edit: { noClickFn: true } }, { cancel: { show: false } }, { delete: { noClickFn: true, show: false } }])
-  },
-  created () {
-    let configTableItem = {
-      selection: 50,
-      fieldValue: { width: 80, clickFn: 'handleGoPage', clsName: 'cm-btn-color', type: 'input', canEdit: 1 },
-      fieldName: { width: 100, type: 'input', canEdit: 1 },
-      fieldRequired: { width: 80, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      displayStatus: { width: 80, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      setStatus: { width: 120, type: 'select', canEdit: 1, options: [{ label: '是', value: 1 }, { label: '否', value: 0 }] },
-      btn: 112
-    }
-    this.tableItem = this.$setItem(tybeObj['tybe-manage2'], configTableItem, 'table')
-    if (this.tableBtn.filter(item => item.show).length === 0) {
-      this.tableItem.splice(this.tableItem.length - 1, 1)
-    }
-    this.rules = {
-      id: [
-        { required: true, message: '请输入菜单id', trigger: 'blur' }
-      ],
-      menuName: [
-        { required: true, message: '请输入菜单名称', trigger: 'blur' }
-      ],
-      menuLevel: [
-        { required: true, message: '请选择菜单类型', trigger: 'change' }
-      ]
-    }
   }
 }
